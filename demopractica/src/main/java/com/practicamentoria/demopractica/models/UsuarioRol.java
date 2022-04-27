22c8a7a689e2d1b3493f5cb8763a2440b45726bb
riveco0120 <richardsk.vellojin@gmail.com>
2022-04-27 16:46:10 -0500

    Entidad UsuarioRol
    Se crea la entidad usuario rol con sus atributos y constructores

---
+++ demopractica/src/main/java/com/practicamentoria/demopractica/models/UsuarioRol.java
@@ -0,0 +1,26 @@
+package com.practicamentoria.demopractica.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Setter
+@Getter
+@Entity
+@Table
+public class UsuarioRol {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true,nullable = false)
+    private long idRol;
+    private String rol;
+    private long idUsuario;
+
+    public UsuarioRol() {
+    }
+
+    public UsuarioRol(String rol) {
+        this.rol = rol;
+    }
+}
