ccc27a851800f6048d8f4f1a2143f1dc479eb02f
riveco0120 <richardsk.vellojin@gmail.com>
2022-04-27 23:49:56 -0500

    Pruebas con mockito

--- demopractica/src/test/java/com/practicamentoria/demopractica/controller/UsuarioRolControllerTest.java
+++ demopractica/src/test/java/com/practicamentoria/demopractica/controller/UsuarioRolControllerTest.java
@@ -1,7 +1,32 @@
 package com.practicamentoria.demopractica.controller;
 
+import com.practicamentoria.demopractica.repositorie.IUsuarioRepository;
+import com.practicamentoria.demopractica.repositorie.IUsuarioRolRepository;
+import com.practicamentoria.demopractica.service.UsuarioRolServicio;
+import com.practicamentoria.demopractica.service.UsuarioService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 class UsuarioRolControllerTest {
+    @MockBean
+    IUsuarioRolRepository usuarioRepository;
+
+    @Autowired
+    UsuarioRolServicio usuarioService;
+
+    @Test
+    public void testUsuarioMock(){
+        when(usuarioRepository.findAll()).thenReturn(new ArrayList<>());
+        assertThat(usuarioService.obtenerUsuariosRol()).isEmpty();
+        verify(usuarioRepository).findAll();
+    }
 
 }
