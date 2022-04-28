7ceeaf6bd7f0336fe1c215eab2b820e5826fd658
riveco0120 <richardsk.vellojin@gmail.com>
2022-04-27 23:57:20 -0500

    Actualizando pruebas mock

--- demopractica/src/test/java/com/practicamentoria/demopractica/controller/UsuarioControllerTest.java
+++ demopractica/src/test/java/com/practicamentoria/demopractica/controller/UsuarioControllerTest.java
@@ -1,18 +1,21 @@
 package com.practicamentoria.demopractica.controller;
 
+import com.practicamentoria.demopractica.models.Usuario;
 import com.practicamentoria.demopractica.repositorie.IUsuarioRepository;
 import com.practicamentoria.demopractica.service.UsuarioService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.ArrayList;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
+@SpringBootTest
 class UsuarioControllerTest {
 
     @MockBean
@@ -26,5 +29,12 @@ class UsuarioControllerTest {
         when(usuarioRepository.findAll()).thenReturn(new ArrayList<>());
         assertThat(usuarioService.obtenerUsuarios()).isEmpty();
         verify(usuarioRepository).findAll();
+       }
+
+    @Test
+    @DisplayName("Test para guardar usuario")
+    public void testGuardar(){
+        var usuario = new Usuario("Richard","rvellojin@gmail.com",2);
+
        }
 }
--- demopractica/src/test/java/com/practicamentoria/demopractica/controller/UsuarioRolControllerTest.java
+++ demopractica/src/test/java/com/practicamentoria/demopractica/controller/UsuarioRolControllerTest.java
@@ -6,6 +6,7 @@ import com.practicamentoria.demopractica.service.UsuarioRolServicio;
 import com.practicamentoria.demopractica.service.UsuarioService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.ArrayList;
@@ -15,6 +16,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+@SpringBootTest
 class UsuarioRolControllerTest {
     @MockBean
     IUsuarioRolRepository usuarioRepository;
